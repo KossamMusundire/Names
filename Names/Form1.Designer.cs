1e5e0a1af66b8f251e1ff2107d52dd59261f0a0a
Kossam Musundire <msnkos001@myuct.ac.za>
2025-07-25 20:46:56 +0200

    Add project files.

---
+++ Names/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Names
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            lstNames = new ListBox();
+            txtName = new TextBox();
+            Names = new Label();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(138, 55);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(100, 23);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Name";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // lstNames
+            // 
+            lstNames.FormattingEnabled = true;
+            lstNames.ItemHeight = 15;
+            lstNames.Location = new Point(12, 27);
+            lstNames.Name = "lstNames";
+            lstNames.Size = new Size(120, 94);
+            lstNames.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(138, 26);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 2;
+            txtName.Text = "Add Name";
+            // 
+            // Names
+            // 
+            Names.AutoSize = true;
+            Names.Location = new Point(12, 9);
+            Names.Name = "Names";
+            Names.Size = new Size(44, 15);
+            Names.TabIndex = 3;
+            Names.Text = "Names";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(252, 141);
+            Controls.Add(Names);
+            Controls.Add(txtName);
+            Controls.Add(lstNames);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "Names";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private ListBox lstNames;
+        private TextBox txtName;
+        private Label Names;
+    }
+}
